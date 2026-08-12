9228ef0b4bc83905bce3fb3858bb2abd24f32c20
kayenforcer <kamil.pukownik@gmail.com>
2026-08-12 10:07:17 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,8 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  // std::cout << "$ ";
+  std::cout << "$ ";
 }
