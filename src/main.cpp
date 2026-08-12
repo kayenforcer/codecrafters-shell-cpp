fd14816d9ba0765bd7a3611472583c505c785b12
kayenforcer <kamil.pukownik@gmail.com>
2026-08-12 11:07:54 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,20 @@ int main()
     {
       break;
     }
-    else if (input.substr(0,5) == "echo ")
+    else if (input.substr(0, 5) == "echo ")
     {
       std::cout << input.substr(5) << "\n";
+    }
+    else if (input.substr(0, 5) == "type ")
+    {
+      if (input.substr(5) == "echo" || input.substr(5) == "exit")
+      {
+        std::cout << input.substr(5) << " is a shell builtin\n";
+      }
+      else
+      {
+        std::cout << input.substr(5) << ": not found\n";
+      }
     }
     else
     {
