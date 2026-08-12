c92d9cb224faf9006e06724cef9067d49c293b7a
kayenforcer <kamil.pukownik@gmail.com>
2026-08-12 10:35:55 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,17 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  while (true)
+  std::cout << "$ ";
+
+  std::string command;
+  std::getline(std::cin, command);
+
+  while (command != "exit")
   {
     std::cout << "$ ";
 
     std::string command;
-    std::cin >> command;
+    std::getline(std::cin, command);
 
     std::cout << command << ": command not found\n";
   }
