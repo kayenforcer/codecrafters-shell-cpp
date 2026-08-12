ee15fd51094fba42b0159eb05771ed55a2f4ba78
kayenforcer <kamil.pukownik@gmail.com>
2026-08-12 12:48:07 +0200

    Builtins done and PATH in progress - split

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
 
 int main()
 {
@@ -25,12 +28,29 @@ int main()
     else if (input.substr(0, 5) == "type ")
     {
       std::string arg = input.substr(5);
+
       if (arg == "echo" || arg == "exit" || arg == "type")
       {
         std::cout << arg << " is a shell builtin\n";
       }
-      else
+
+      else // tutaj robimy
       {
+        std::vector<std::string> paths;
+        const char *pathvar_env = std::getenv("PATH");
+        if (pathvar_env != nullptr)
+        {
+          std::string pathvar(pathvar_env);
+          std::stringstream ss(pathvar);
+          std::string token;
+          char delimiter = ':';
+
+          while (std::getline(ss, token, delimiter))
+          {
+            paths.push_back(token);
+          }
+        }
+
         std::cout << arg << ": not found\n";
       }
     }
